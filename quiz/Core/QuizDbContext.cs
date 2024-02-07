69efa564ebccb200ae17cfee6f2256e607c1f6e7
Olav Sørlie <olav.sorlie@gmail.com>
2024-02-07 15:42:52 +0100

    Added DbContext

---
+++ quiz/Core/QuizDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using quiz.Repositories.Models;
+
+namespace quiz.Core
+{
+    public class QuizDbContext : DbContext
+    {
+        public DbSet<AlternativeModel> Alternatives { get; set; }
+
+        public DbSet<QuestionModel> Questions { get; set; }
+
+        public QuizDbContext(DbContextOptions<QuizDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<QuestionModel>()
+                .HasMany(x => x.Alternatives)
+                .WithOne(x => x.Question)
+                .HasForeignKey(x => x.QuestionId)
+                .HasPrincipalKey(x => x.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
