eb2ed7cea078aa4803ecdb5ad258f0f24ea5150e
Olav Sørlie <olav.sorlie@gmail.com>
2024-02-07 15:52:43 +0100

    Added dependency injection for controller, service, repository and database

---
+++ quiz/Core/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using quiz.Repositories;
+using quiz.Services;
+
+namespace quiz.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IQuestionService, QuestionService>();
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IQuestionRepository, QuestionRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<QuizDbContext>(options =>
+                options.UseSqlServer("Server=localhost\\MSSQLSERVER02;Database=master;Trusted_Connection=True;")
+            );
+            return services;
+        }
+    }
+}
--- quiz/Program.cs
+++ quiz/Program.cs
@@ -1,8 +1,14 @@
+using quiz.Core;
+
+var allowedOrigins = "_allowedOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddServices();
+builder.Services.AddRepositories();
+builder.Services.AddDatabase();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
