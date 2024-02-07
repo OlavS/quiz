aca98d5dafbd34f81c832fbf68bcd5eba8b67ca3
Olav Sørlie <olav.sorlie@gmail.com>
2024-02-07 15:44:39 +0100

    Added QuestionDto and AlternativeDto

---
+++ quiz/Dtos/AlternativeDto.cs
@@ -0,0 +1,17 @@
+﻿using quiz.Repositories.Models;
+
+namespace quiz.Dtos
+{
+    public class AlternativeDto
+    {
+        public AlternativeDto(AlternativeModel alternative)
+        {
+            Alternative = alternative.Alternative;
+            CorrectAnswer = alternative.CorrectAnswer;
+        }
+
+        public string Alternative { get; set; }
+
+        public bool CorrectAnswer { get; set; }
+    }
+}
---
+++ quiz/Dtos/QuestionDto.cs
@@ -0,0 +1,18 @@
+﻿using quiz.Repositories.Models;
+using System.Security;
+
+namespace quiz.Dtos
+{
+    public class QuestionDto
+    {
+        public QuestionDto(QuestionModel question)
+        {
+            Question = question.Question;
+            Alternatives = question.Alternatives.Select(it => new AlternativeDto(it));
+        }
+
+        public string Question { get; set; }
+
+        public IEnumerable<AlternativeDto> Alternatives { get; set; }
+    }
+}
