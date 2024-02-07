e33ee4748777af1d21849fdfc8980a77af34dc28
Olav Sørlie <olav.sorlie@gmail.com>
2024-02-07 15:39:38 +0100

    Added db models

---
+++ quiz/Repositories/Models/AlternativeModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace quiz.Repositories.Models
+{
+    [Table("Alternative")]
+    public class AlternativeModel : Entity
+    {
+        public string Alternative { get; set; }
+
+        public bool CorrectAnswer { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public QuestionModel Question { get; set; }
+    }
+}
---
+++ quiz/Repositories/Models/QuestionModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace quiz.Repositories.Models
+{
+    [Table("Question")]
+    public class QuestionModel : Entity
+    {
+        public string Question { get; set; }
+
+        public IEnumerable<AlternativeModel> Alternatives { get; set; } 
+    }
+}
